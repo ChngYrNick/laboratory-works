09ec70c4482bb817d72b39aba2e39f93896a41ea
ChngYrNick <45335045+ChngYrNick@users.noreply.github.com>
2019-05-26 16:50:13 +0300

    commit 1

--- src/functions/factorial.js
+++
@@ -1,8 +0,0 @@
-export default function factorial(num) {
-  let res = 1;
-  while (num > 0) {
-    res *= num;
-    --num;
-  }
-  return res;
-}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import program from "commander";
-import backwardInterpolation from "./functions/backwardInterpolation";
-import forwardInterpolation from "./functions/forwardInterpolation";
-import lagrangeMethod from "./functions/lagrangeMethod";
 
 program
   .version("0.0.1")
   .description("Application for solving interpolation tasks")
-  .option("-f, --forward-interpolation", "Interpolation 'Forward'")
-  .option("-b, --backward-interpolation", "Interpolation 'Backward'")
-  .option("-l, --lagrange-method", "Lagrange method")
+  .option("-s, --spline-interpolation", "Spline interpolation")
   .parse(process.argv);
 
 const t = 3.5;
@@ -16,15 +11,3 @@ const t = 3.5;
 const x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const y = [-1, 2, 17, 50, 107, 194, 317, 482, 695, 962, 1289];
-
-if (program.forwardInterpolation) {
-  console.log(`Interpolation 'Forward': ${forwardInterpolation(x, y, t)}`);
-}
-
-if (program.backwardInterpolation) {
-  console.log(`Interpolation 'Backward': ${backwardInterpolation(x, y, t)}`);
-}
-
-if (program.lagrangeMethod) {
-  console.log(`Lagrange method: ${lagrangeMethod(x, y, t)}`);
-}
